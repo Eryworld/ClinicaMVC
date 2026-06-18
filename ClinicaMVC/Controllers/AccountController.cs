034937c6fb7e7c0bed5f368a8ad20eed19a3bb4b
Eryworld <117729506+Eryworld@users.noreply.github.com>
2026-06-18 09:56:16 -0400

    Add project files.

---
+++ ClinicaMVC/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicaMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string usuario, string password)
+        {
+            // Temporal para pruebas
+
+            if (usuario == "admin" && password == "1234")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+
+            return View();
+        }
+    }
+}
